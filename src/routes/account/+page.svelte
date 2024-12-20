5c3ff763961252f945bdd197eb1542bf60f17d6a
romain.gyh <11901536+romaingyh@users.noreply.github.com>
2024-12-20 01:05:17 +0100

    fix: don't show pricing table if no userId

--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -61,15 +61,18 @@
 		{/if}
 
 		{#if !subscription || subscription.status === 'canceled'}
-			<div class="mb-4" />
-
-			<script async src="https://js.stripe.com/v3/pricing-table.js"></script>
-			<stripe-pricing-table
-				pricing-table-id={PUBLIC_STRIPE_PRICING_TABLE_ID}
-				publishable-key={PUBLIC_STRIPE_PUBLISHABLE_KEY}
-				client-reference-id={user.id}
-			>
-			</stripe-pricing-table>
+			{#if user.id}
+				<script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+				<stripe-pricing-table
+					pricing-table-id={PUBLIC_STRIPE_PRICING_TABLE_ID}
+					publishable-key={PUBLIC_STRIPE_PUBLISHABLE_KEY}
+					client-reference-id={user.id}
+				>
+				</stripe-pricing-table>
+			{:else}
+				<p>Connectez-vous pour acheter un abonnement</p>
+				<FilledButton href="/auth">Connexion</FilledButton>
+			{/if}
 		{/if}
 	</section>
 </div>
